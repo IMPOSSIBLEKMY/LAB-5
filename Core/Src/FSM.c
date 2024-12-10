51823ba77cbe4a3ff136c5b347981a496fc42dd1
DESKTOP-D444S2V\NTKM <impossiblekmy12345@gmail.com>
2024-12-10 18:42:57 +0700

    Update

---
+++ Core/Src/FSM.c
@@ -0,0 +1,12 @@
+/*
+ * FSM.c
+ *
+ *  Created on: Dec 10, 2024
+ *      Author: NTKM
+ */
+
+#include "FSM.h"
+
+uint8_t buffer[MAX_BUFFER_SIZE];
+uint8_t index_buffer = 0;
+uint8_t buffer_flag = 0;
